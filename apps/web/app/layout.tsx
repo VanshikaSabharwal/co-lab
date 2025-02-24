c4ea9afa230ce5640fcf0c39a5de4213feea00aa
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2025-02-24 20:30:55 +0530

    corrected name/title to Ko-Lab

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Ko-Lab,
+  title: "Ko-Lab",
   description: "Collaborative code Editor with group chat and one to one chat functionalities ",
 };
 
